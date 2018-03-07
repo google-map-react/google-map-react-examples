2435b53aaa4e1cefb4fb6b9d9f69f44146c7b8db
Michael Diego <itsmichaeldiego@gmail.com>
2018-03-07 00:51:56 -0800

    Prettify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app" />
-    );
-  }
-}
+const App = () => (
+  <div className="app" />
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// styles
 import './index.css';
+
+// components
 import App from './App';
+
+// utils
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
