c87ba26bcae3fc1890cba2209df82c06fbb52549
Michael Diego <itsmichaeldiego@gmail.com>
2018-03-07 00:59:28 -0800

    Add basic GoogleMapReact

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+
 import './App.css';
 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
 const App = () => (
-  <div className="app" />
+  <div className="app">
+    <GoogleMapReact
+      bootstrapURLKeys={{
+        key: process.env.REACT_APP_MAP_KEY
+      }}
+      defaultCenter={[33.9850, -118.4695]}
+      defaultZoom={14}
+    >
+      <AnyReactComponent
+        lat={33.98732}
+        lng={-118.4732217}
+        text={'Samesun Venice Beach Hostel'}
+      />
+    </GoogleMapReact>
+  </div>
 );
 
 export default App;
