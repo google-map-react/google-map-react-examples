39d560c3c0d2d8964a55747ad90f931452c4b425
Michael Diego <itsmichaeldiego@gmail.com>
2018-03-07 16:41:33 -0800

    Add eslint

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -4,34 +4,38 @@ import GoogleMapReact from 'google-map-react';
 
 import './Map.css';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const Marker = ({ text }) => <div>{text}</div>;
+
+Marker.propTypes = {
+  text: PropTypes.string.isRequired,
+};
 
 const Map = ({ center, zoom }) => (
   <div className="map">
     <GoogleMapReact
       bootstrapURLKeys={{
-        key: process.env.REACT_APP_MAP_KEY
+        key: process.env.REACT_APP_MAP_KEY,
       }}
       defaultCenter={center}
       defaultZoom={zoom}
     >
-      <AnyReactComponent
+      <Marker
         lat={33.98732}
         lng={-118.4732217}
-        text={'Samesun Venice Beach Hostel'}
+        text="Samesun Venice Beach Hostel"
       />
     </GoogleMapReact>
   </div>
 );
 
-Map.defaultProps = {
+Map.propTypes = {
   center: PropTypes.arrayOf(PropTypes.number),
   zoom: PropTypes.number,
-}
+};
 
 Map.defaultProps = {
   center: [33.9850, -118.4695],
   zoom: 14,
-}
+};
 
 export default Map;
