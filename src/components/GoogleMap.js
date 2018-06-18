897b49c2374ccd53d0b10ce2867e81bf83678371
Michael Diego <6419886+itsmichaeldiego@users.noreply.github.com>
2018-06-18 18:20:51 +0300

    Fetch places (#6)
    * Use google's api data
    
    * Move markers logic to Main module
    
    * Use fetch in componentDidMount instead of requiring files locally
    
    * Fit bounds on map load
    
    * Resize map when resizing window

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -3,45 +3,33 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import GoogleMapReact from 'google-map-react';
 
-// components:
-import Marker from './Marker';
-
-import PLACES from '../data/places.json';
-
 const Wrapper = styled.main`
   width: 100%;
   height: 100%;
 `;
 
-const GoogleMap = ({ center, zoom }) => (
+const GoogleMap = ({ children, ...props }) => (
   <Wrapper>
     <GoogleMapReact
       bootstrapURLKeys={{
         key: process.env.REACT_APP_MAP_KEY,
       }}
-      defaultCenter={center}
-      defaultZoom={zoom}
+      {...props}
     >
-      {PLACES.businesses.map(place => (
-        <Marker
-          key={place.id}
-          text={place.name}
-          lat={place.coordinates.latitude}
-          lng={place.coordinates.longitude}
-        />
-      ))}
+      {children}
     </GoogleMapReact>
   </Wrapper>
 );
 
 GoogleMap.propTypes = {
-  center: PropTypes.arrayOf(PropTypes.number),
-  zoom: PropTypes.number,
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
 };
 
 GoogleMap.defaultProps = {
-  center: [PLACES.region.center.latitude, PLACES.region.center.longitude],
-  zoom: 10,
+  children: null,
 };
 
 export default GoogleMap;
