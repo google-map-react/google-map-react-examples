3b80fa6d9227cabe9db66c5b32a1f8d31311b21d
Michael Diego <itsmichaeldiego@gmail.com>
2018-06-18 17:59:58 +0300

    Use fetch in componentDidMount instead of requiring files locally

--- src/modules/Main.js
+++ src/modules/Main.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 // components:
 import Marker from '../components/Marker';
 import GoogleMap from '../components/GoogleMap';
 
-import PLACES from '../data/places.json';
 import LOS_ANGELES_CENTER from '../const/la_center';
 
 const Wrapper = styled.section`
@@ -13,22 +12,43 @@ const Wrapper = styled.section`
   height: 100vh;
 `;
 
-const Main = () => (
-  <Wrapper>
-    <GoogleMap
-      defaultCenter={LOS_ANGELES_CENTER}
-      defaultZoom={10}
-    >
-      {PLACES.results.map(place => (
-        <Marker
-          key={place.id}
-          text={place.name}
-          lat={place.geometry.location.lat}
-          lng={place.geometry.location.lng}
-        />
-      ))}
-    </GoogleMap>
-  </Wrapper>
-);
+class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      places: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('/places.json')
+      .then(response => response.json())
+      .then(data => this.setState({ places: data.results }));
+  }
+
+  render() {
+    const { places } = this.state;
+    return (
+      <Wrapper>
+        {places && (
+          <GoogleMap
+            defaultCenter={LOS_ANGELES_CENTER}
+            defaultZoom={10}
+          >
+            {places.map(place => (
+              <Marker
+                key={place.id}
+                text={place.name}
+                lat={place.geometry.location.lat}
+                lng={place.geometry.location.lng}
+              />
+            ))}
+          </GoogleMap>
+        )}
+      </Wrapper>
+    );
+  }
+}
 
 export default Main;
