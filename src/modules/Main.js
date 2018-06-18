3f12ae6dd5fa0676598cdf6565933c7ed52df9a0
Michael Diego <itsmichaeldiego@gmail.com>
2018-06-18 18:11:53 +0300

    Resize map when resizing window

--- src/modules/Main.js
+++ src/modules/Main.js
@@ -26,12 +26,23 @@ const getMapBounds = (map, maps, places) => {
   return bounds;
 };
 
+// Re-center map when resizing the window
+const bindResizeListener = (map, maps, bounds) => {
+  maps.event.addDomListenerOnce(map, 'idle', () => {
+    maps.event.addDomListener(window, 'resize', () => {
+      map.fitBounds(bounds);
+    });
+  });
+};
+
 // Fit map to its bounds after the api is loaded
 const apiIsLoaded = (map, maps, places) => {
   // Get bounds by our places
   const bounds = getMapBounds(map, maps, places);
   // Fit map to bounds
   map.fitBounds(bounds);
+  // Bind the resize listener
+  bindResizeListener(map, maps, bounds);
 };
 
 class Main extends Component {
