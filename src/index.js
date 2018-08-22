b19f5222c9b8058eccb5c8b88c9bcc70c45f330a
Michael Diego <6419886+itsmichaeldiego@users.noreply.github.com>
2018-08-22 22:19:51 +0200

    Multiple examples (#10)
    * Create folder examples
    
    * Add react-router-dom
    
    * Add comment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  Route,
+  Switch,
+  Redirect,
+  BrowserRouter as Router,
+} from 'react-router-dom';
+
+// examples:
+import Main from './examples/Main';
 
 // styles
 import './index.css';
@@ -10,5 +19,17 @@ import App from './App';
 // utils
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        {/* New examples here */}
+        <Redirect exact from="*" to="/" />
+      </Switch>
+    </App>
+  </Router>,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
