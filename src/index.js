92bcd282e47576215b0f0b38c49ab421a7f568ab
Callam Williams <lhocdesign@gmail.com>
2018-11-16 13:57:55 +0000

    [autocomplete] - added basic autocomplete example

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Switch, Redirect, BrowserRouter as Router } from 'react-router-dom';
+import {
+  Route,
+  Switch,
+  Redirect,
+  BrowserRouter as Router
+} from 'react-router-dom';
 
 // examples:
 import Home from './Home';
 import Main from './examples/Main';
 import SearchBox from './examples/Searchbox';
+import Autocomplete from './examples/Autocomplete';
 
 // styles
 import './index.css';
@@ -26,11 +32,12 @@ ReactDOM.render(
         {/* New examples here */}
         <Route path={`${defaultPath}default`} component={Main} />
         <Route path={`${defaultPath}searchbox`} component={SearchBox} />
+        <Route path={`${defaultPath}autocomplete`} component={Autocomplete} />
         <Redirect exact from="*" to={defaultPath} />
       </Switch>
     </App>
   </Router>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
 
 registerServiceWorker();
