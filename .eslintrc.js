a7aae7246797701999e07d49ed486d9aec6c720e
Callam Williams <lhocdesign@gmail.com>
2018-11-19 11:28:22 +0000

    [feature/pre-lint] - stronger config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   "parser": "babel-eslint",
-  "extends": "airbnb",
+  "extends": ["airbnb", "prettier"],
+  "plugins": ["prettier"],
   "rules": {
+    "prettier/prettier": ["error"],
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
   },
   "env": {
