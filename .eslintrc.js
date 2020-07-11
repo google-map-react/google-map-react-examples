49c2a9ae47ce4a3099ea2742e53790dc11ab519f
Michael <itsmichaeldiego@gmail.com>
2020-07-11 00:33:46 -0300

    Fix warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,15 @@ module.exports = {
   "extends": "airbnb",
   "rules": {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          ".storybook/**",
+          "src/stories/**",
+        ]
+      }
+    ]
   },
   "env": {
     "es6": true,
